626b33c72b9308986a7a3b812cbab60a290ba800
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-08-01 16:12:28 +0000

    poniendo el codigo dentro de windows.load

--- src/app.js
+++ src/app.js
@@ -5,28 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "The mailman", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "my phone", "the car"];
-let when = [
-  "before the class",
-  "when I was sleeping",
-  "while I was exercising",
-  "during my lunch",
-  "while I was praying"
-];
-
-// 0.99 / 1 math.floor 0
-// 0.4124124123123124
-
-function randomGenerator(arr) {
-  let random = Math.floor(Math.random() * arr.length);
-
-  return arr[random];
-}
-
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function randomGenerator(arr) {
+    let random = Math.floor(Math.random() * arr.length);
+
+    return arr[random];
+  }
+
   console.log("Hello Rigo from the console!");
 
   let excuse = `${randomGenerator(who)} ${randomGenerator(
