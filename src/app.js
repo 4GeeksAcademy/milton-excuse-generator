74d4a1c4b4927404fa339887c7659897fca243d1
Amat98 <108639529+Amat98@users.noreply.github.com>
2024-08-01 16:09:21 +0000

    creando un generador de excusas y poniendolo en el html

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+// 0.99 / 1 math.floor 0
+// 0.4124124123123124
+
+function randomGenerator(arr) {
+  let random = Math.floor(Math.random() * arr.length);
+
+  return arr[random];
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let excuse = `${randomGenerator(who)} ${randomGenerator(
+    action
+  )} ${randomGenerator(what)} ${randomGenerator(when)}`;
+  document.getElementById("excuse").innerHTML = excuse;
 };
